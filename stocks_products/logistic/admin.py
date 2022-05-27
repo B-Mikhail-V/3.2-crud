6efb9d1e2abdf6112da50b3e380cb364482723e1
Mikail Bakulin <bakulin.mv@gmail.com>
2022-05-27 16:56:25 +0300

    2222

--- stocks_products/logistic/admin.py
+++ stocks_products/logistic/admin.py
@@ -3,14 +3,16 @@ from django.contrib import admin
 from logistic.models import StockProduct, Product, Stock
 
 
+
 class StockProductInline(admin.TabularInline):
     model = StockProduct
-    # formset = StockProductInlineFormset
     extra = 0
 
 @admin.register(Product)
 class ArticleAdmin(admin.ModelAdmin):
     inlines = [StockProductInline]
+
+
 
 @admin.register(Stock)
 class ArticleAdmin(admin.ModelAdmin):
