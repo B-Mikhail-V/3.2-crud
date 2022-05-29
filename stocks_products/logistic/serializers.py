57b3578f1dab9ea5c036c1805cd5c212d443ba67
B-Mikhail-V <bakulin.mv@gmail.com>
2022-05-29 22:47:37 +0300

    5555

--- stocks_products/logistic/serializers.py
+++ stocks_products/logistic/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+
 from logistic.models import Product, StockProduct, Stock
 
 
@@ -7,15 +8,15 @@ class ProductSerializer(serializers.ModelSerializer):
     # настройте сериализатор для продукта
     class Meta:
         model = Product
-        fields = ['title', 'description']
+        fields = ['id', 'title', 'description']
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
     # настройте сериализатор для позиции продукта на складе
-    product = ProductSerializer()
+    # product = ProductSerializer()
     class Meta:
         model = StockProduct
-        fields = ['stock', 'product', 'quantity', 'price']
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
@@ -26,11 +27,19 @@ class StockSerializer(serializers.ModelSerializer):
         fields = ['address', 'positions']
 
     def create(self, validated_data):
+
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
 
         # создаем склад по его параметрам
         stock = super().create(validated_data)
+
+        for pos in positions:
+            StockProduct.objects.create(stock=stock, product=pos['product'], quantity=pos['quantity'],price=pos['price'])
+            StockProduct.objects.create(stock=stock, **pos)
+
+
+
 
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
@@ -38,12 +47,43 @@ class StockSerializer(serializers.ModelSerializer):
 
         return stock
 
+
     def update(self, instance, validated_data):
+        # print(instance)
+        # print(self)
+        # print(validated_data)
+
         # достаем связанные данные для других таблиц
         positions = validated_data.pop('positions')
+        print(positions)
+        for pos in positions:
+            print(pos['product'])
+            print(pos['quantity'])
+            print(pos['price'])
 
+        # print(validated_data)
         # обновляем склад по его параметрам
         stock = super().update(instance, validated_data)
+        # print(stock)
+        # self.data.update(positions)
+        # print(StockSerializer.validated_data[:])
+        # self.data.update(['positions'='new'])
+        # new_dict = dict(positions=positions)
+        # print(new_dict)
+
+        # st = StockProduct.objects.filter(stock=stock)
+        # print(st.values())
+        # for ss in st.values():
+        #     print(ss)
+        #     for pos in positions:
+        #         StockProduct.objects.update(product=pos['product'])
+        #         StockProduct.objects.update(quantity=pos['quantity'])
+        #         StockProduct.objects.update(price=pos['price'])
+
+
+
+
+
 
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
